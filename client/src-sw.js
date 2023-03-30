b40c7750314e5316a729fbfb9c260b59ab8a4dad
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-03-29 17:20:12 -0700

    added service worker and caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,14 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -20,11 +20,25 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
+// Once the fetch occurs on the cache (self), our "page-cache" is opened and searched for what is being fetched. If a match is found it is returned. If no match is found within the cache the network is then searched for a match and returned. Both the cache and the network are searched when online. When offline, the cache is used.
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.open(page-cache).then(function (cache) {
+      cache.match(event.request).then(function (cacheResponse) {
+        fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse);
+        });
+        return cacheResponse || networkResponse;
+      });
+    })
+  );
+});
+
 registerRoute();
