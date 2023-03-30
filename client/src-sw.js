028d6252ccd9a1d3d1c6b8b78869e59a4aea8aad
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-03-29 18:11:55 -0700

    updated asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,19 +26,31 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
+//Implementing asset caching
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("page-cache2");
+  await cache.addAll(resources);
+};
+
+// The cache is not installed until all the information from the "addResourcesToCache" data is retrieved, then the cache is installed and able to be used.
+self.addEventListener("install", (event) => {
+  event.waitUntil(addResourcesToCache(["/src", "/favicon.ico", "/index.html"]));
+});
+
 // Once the fetch occurs on the cache (self), our "page-cache" is opened and searched for what is being fetched. If a match is found it is returned. If no match is found within the cache the network is then searched for a match and returned. Both the cache and the network are searched when online. When offline, the cache is used.
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.open(page-cache).then(function (cache) {
-      cache.match(event.request).then(function (cacheResponse) {
-        fetch(event.request).then(function (networkResponse) {
-          cache.put(event.request, networkResponse);
+self.addEventListener('fetch', async (event) => {
+    // Open the cache
+    event.respondWith(caches.open("page-cache").then((cache) => {
+      // Respond with match from the cache or from the network
+      return cache.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+          // If the response is from the network, it is saved in the cache.
+          cache.put(event.request, fetchedResponse.clone());
+          // Return the network response if using the network response.
+          return fetchedResponse;
         });
-        return cacheResponse || networkResponse;
       });
-    })
-  );
+    }));
 });
 
 registerRoute();
